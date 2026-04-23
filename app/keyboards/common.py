7a6ccf52b5fe75dd2f9590fc8aaf74ebf090a250
1georgmoscow-cmd <1georg.moscow@gmail.com>
2026-04-24 00:40:57 +0300

    Update common.py

--- app/keyboards/common.py
+++ app/keyboards/common.py
@@ -38,6 +38,6 @@ def subscription_kb(channel_link: str) -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(
         inline_keyboard=[
             [InlineKeyboardButton(text="Подписаться", url=channel_link)],
-            [InlineKeyboardButton(text="Проверить подписку", callback_data="check_subscription")],
+            [InlineKeyboardButton(text="Проверить подписку", callback_data="check_sub"))],
         ]
     )
