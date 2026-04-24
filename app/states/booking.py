6d6b561e5de121d844fa2efa1784fb653a9781f9
1georgmoscow-cmd <1georg.moscow@gmail.com>
2026-04-24 15:00:49 +0300

    Update booking.py

--- app/states/booking.py
+++ app/states/booking.py
@@ -2,28 +2,5 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class BookingStates(StatesGroup):
-    # =========================
-    # выбор услуги
-    # =========================
-    choosing_service = State()
-
-    # =========================
-    # выбор даты
-    # =========================
-    choosing_date = State()
-
-    # =========================
-    # выбор времени
-    # =========================
-    choosing_time = State()
-
-    # =========================
-    # ввод данных клиента
-    # =========================
     waiting_for_name = State()
     waiting_for_phone = State()
-
-    # =========================
-    # подтверждение записи
-    # =========================
-    confirmation = State()
