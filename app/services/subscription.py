e51125107cce589aacbc6ee59c99b59081659c07
1georgmoscow-cmd <1georg.moscow@gmail.com>
2026-04-24 17:31:25 +0300

    Update subscription.py

--- app/services/subscription.py
+++ app/services/subscription.py
@@ -1,17 +1,18 @@
 from aiogram import Bot
-from aiogram.enums import ChatMemberStatus
+from aiogram.exceptions import TelegramBadRequest
 
 
-async def is_subscribed(bot: Bot, channel_id: int, user_id: int) -> bool:
+async def is_subscribed(bot: Bot, channel_id: str | int, user_id: int) -> bool:
     try:
-        member = await bot.get_chat_member(chat_id=channel_id, user_id=user_id)
-
-        return member.status in (
-            ChatMemberStatus.MEMBER,
-            ChatMemberStatus.ADMINISTRATOR,
-            ChatMemberStatus.CREATOR,
+        member = await bot.get_chat_member(
+            chat_id=channel_id,
+            user_id=user_id,
         )
 
-    except Exception as e:
-        print("SUB CHECK ERROR:", e)
+        return member.status in ("member", "administrator", "creator")
+
+    except TelegramBadRequest:
+        return False
+
+    except Exception:
         return False
