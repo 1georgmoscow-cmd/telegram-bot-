94f52930ce13127d00e59322c13e0063a0d0676e
1georgmoscow-cmd <1georg.moscow@gmail.com>
2026-04-24 11:19:46 +0300

    Update booking.py

--- app/handlers/booking.py
+++ app/handlers/booking.py
@@ -46,7 +46,7 @@ async def show_calendar(message, db: Database):
 # =========================
 # 📌 КНОПКА "ЗАПИСАТЬСЯ"
 # =========================
-@router.callback_query(StateFilter(None), F.data == "start_booking")
+@router.callback_query(F.data.in_(["start_booking", "book"]))
 async def start_booking(callback: CallbackQuery, bot: Bot, settings: Settings, db: Database):
     try:
         await callback.answer()
